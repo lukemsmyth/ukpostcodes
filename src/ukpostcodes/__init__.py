93d020626d1613f6cce9ed0809f713482741b813
Luke Smyth <luke-smyth@outlook.com>
2025-07-11 22:36:00 +0100

    test pre-commit

--- src/ukpostcodes/__init__.py
+++ src/ukpostcodes/__init__.py
@@ -2,5 +2,4 @@ def hello() -> str:
     return "Hello from ukpostcodes!"
 
 def too_long():
-    if "abcdefghijklmnopqrstuvwxyz" and 1234567890 and "abcdefghijklmnopqrstuvwxyz" and 1234567890:
-        print("Too long")
+    x = ["warning:", "in", "the", "working", "copy", "of", "'src/ukpostcodes/__init__.py',", "LF", "will", "be", "replaced", "by", "CRLF", "the", "next", "time", "Git", "touches", "it"]
