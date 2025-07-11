05044873f3ce988ede39da4b24819c486090127b
Luke Smyth <luke-smyth@outlook.com>
2025-07-11 22:39:38 +0100

    revert chnges made for pre-commit test

--- src/ukpostcodes/__init__.py
+++ src/ukpostcodes/__init__.py
@@ -1,5 +1,2 @@
 def hello() -> str:
     return "Hello from ukpostcodes!"
-
-def too_long():
-    x = ["warning:", "in", "the", "working", "copy", "of", "'src/ukpostcodes/__init__.py',", "LF", "will", "be", "replaced", "by", "CRLF", "the", "next", "time", "Git", "touches", "it"]
