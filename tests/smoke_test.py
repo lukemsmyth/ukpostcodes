dc3b8bcb27606d580b5ccac46011e97bc09f2021
Luke Smyth <luke-smyth@outlook.com>
2025-07-16 07:43:15 +0100

    chore: add a number of tests.

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -10,3 +10,15 @@ def test_validate_returns_true_for_valid_postcode():
 
 def test_validate_returns_false_for_invalid_postcode():
     assert not validate(INVALID_POSTCODE)
+
+
+def test_validate_returns_true_for_valid_postcode_without_whitespace():
+    assert validate("DE78HQ")
+
+
+def test_validate_returns_true_for_valid_postcode_in_lowercase():
+    assert validate("de78hq")
+
+def test_validate_returns_true_for_valid_xx_postcode():
+    xx_postcode = "XX40 4AA"
+    assert validate(xx_postcode)
